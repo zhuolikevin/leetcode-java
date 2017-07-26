1eac58317d827fa54bd2b4cb537db34f891d6441
lizhuo <lizhuogo@gmail.com>
2017-07-26 16:23:27 -0500

    7.26 problems

---
+++ 282.expression-add-operators.111272610.ac.java
@@ -0,0 +1,47 @@
+/*
+ * [282] Expression Add Operators
+ *
+ * https://leetcode.com/problems/expression-add-operators
+ *
+ * Hard (29.54%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '"123"\n6'
+ *
+ * Can you solve this problem? 🤔
+ */
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if (num == null || num.length() == 0) {
+            return res;
+        }
+        
+        helper(res, num, "", (long)target, 0, 0, 0);
+        return res;
+    }
+    
+    private void helper(List<String> res, String num, String path, long target, int start, long eval, long multiple) {
+        if (start == num.length()) {
+            if (eval == target) {
+                res.add(path);
+                return;
+            }
+        }
+        
+        for (int i = start; i < num.length(); i++) {
+            if (i > start && num.charAt(start) == '0') {
+                break;
+            }
+            
+            long number = Long.parseLong(num.substring(start, i + 1));
+            if (start == 0) {
+                helper(res, num, path + number, target, i + 1, number, number);
+            } else {
+                helper(res, num, path + "+" + number, target, i + 1, eval + number, number);
+                helper(res, num, path + "-" + number, target, i + 1, eval - number, -number);
+                helper(res, num, path + "*" + number, target, i + 1, eval - multiple + multiple * number, multiple * number);
+            }
+        }
+    }
+}
---
+++ 296.best-meeting-point.111273572.ac.java
@@ -0,0 +1,63 @@
+/*
+ * [296] Best Meeting Point
+ *
+ * https://leetcode.com/problems/best-meeting-point
+ *
+ * Hard (51.59%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '[[1,0,0,0,1],[0,0,0,0,0],[0,0,1,0,0]]'
+ *
+ * Can you solve this problem? 🤔
+ */
+class Point {
+    int x;
+    int y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+public class Solution {
+    public int minTotalDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        List<Point> persons = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    persons.add(new Point(i, j));
+                }
+            }
+        }
+        
+        Collections.sort(persons, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                return p1.x - p2.x;
+            }
+        });
+        
+        int x = persons.get(persons.size() / 2).x;
+        
+        Collections.sort(persons, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                return p1.y - p2.y;
+            }
+        });
+        
+        int y = persons.get(persons.size() / 2).y;
+        
+        int sum = 0;
+        for (Point person : persons) {
+            sum += Math.abs(x - person.x) + Math.abs(y - person.y);
+        }
+        
+        return sum;
+    }
+}
